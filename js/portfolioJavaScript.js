a8ad5900f6634cae0e1248032df64e1fdd528756
Steve Schaner <schanerst@gmail.com>
2018-01-23 14:03:43 -0500

    progress bars on scroll on desktop

--- js/portfolioJavaScript.js
+++ js/portfolioJavaScript.js
@@ -2,10 +2,6 @@
 VARIABLES
 **********************************/
 
-const $window = $(window),
-      win_height_padded = $window.height() * 1.1,
-      isTouch = Modernizr.touch;
-
 const openSideMenu = (event) => {
   document.getElementById('side-menu').style.width = '250px'
   event.preventDefault()
@@ -51,30 +47,33 @@ document.getElementById('profile-pic-lg-2').addEventListener('mouseout', steve);
 PROGESS BARS
 **********************************/
 
-showSkills = function(index, percentage) {
-  skillsbar.addEventListener('mouseover', () => {
-    progressbar[index].style.width = percentage + "%";
-    progressbar[index].innerHTML = percentage + "%";
-  });
+progressBarScroll = function() {
+  var ypos = window.pageYOffset;
+
+  if(ypos > 1198) {
+    showSkills = function(index, percentage) {
+        progressbar[index].style.width = percentage + "%";
+        progressbar[index].innerHTML = percentage + "%";
+    }
+    showSkills(0,80);
+    showSkills(1,75);
+    showSkills(2,60);
+    showSkills(3,90);
+    showSkills(4,90);
+    showSkills(5,90);
+  }
+  else {
+    hideSkills = function(index, percentage) {
+        progressbar[index].style.width = percentage + "%";
+        progressbar[index].innerHTML = "";
+    }
+    hideSkills(0, 0);
+    hideSkills(1, 0);
+    hideSkills(2, 0);
+    hideSkills(3, 0);
+    hideSkills(4, 0);
+    hideSkills(5, 0);
+  }
 }
 
-hideSkills = function(index, percentage) {
-  skillsbar.addEventListener('mouseout', () => {
-    progressbar[index].style.width = percentage + "%";
-    progressbar[index].innerHTML = "";
-  });
-}
-
-showSkills(0,80);
-showSkills(1,75);
-showSkills(2,60);
-showSkills(3,90);
-showSkills(4,90);
-showSkills(5,90);
-
-hideSkills(0, 0);
-hideSkills(1, 0);
-hideSkills(2, 0);
-hideSkills(3, 0);
-hideSkills(4, 0);
-hideSkills(5, 0);
+window.addEventListener("scroll", progressBarScroll)
